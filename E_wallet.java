97de82ae51d11dc2ac461353342e0032cda1162b
Sahar <100488384+SaharMFR@users.noreply.github.com>
2023-05-13 18:49:45 +0300

    E_wallet class

---
+++ E_wallet.java
@@ -0,0 +1,14 @@
+public class E_wallet implements PaymentMethods {
+    @Override
+    public void pay(float amount) {
+
+    }
+
+    public boolean verifyNumber(int walletNumber) {
+        return true;
+    }
+
+    public void checkOut() {
+
+    }
+}
