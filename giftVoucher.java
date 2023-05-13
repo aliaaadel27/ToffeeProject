7ad314ab1ae6bc0b9dcd713e0c88a85233ac87ac
Sahar <100488384+SaharMFR@users.noreply.github.com>
2023-05-13 18:50:37 +0300

    giftVoucher class

---
+++ giftVoucher.java
@@ -0,0 +1,16 @@
+public class giftVoucher implements PaymentMethods {
+    private String code;
+
+    @Override
+    public void pay(float amount) {
+
+    }
+
+    public void getVoucher() {
+
+    }
+
+    public void checkOut() {
+
+    }
+}
